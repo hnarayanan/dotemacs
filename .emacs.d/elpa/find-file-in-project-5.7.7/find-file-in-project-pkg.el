6744fb3e9239c9b2ee52d05f10bfbdbfb4c349a7
Harish Narayanan <hnarayanan@gmail.com>
2019-12-16 17:18:35 +0000

    Revert "Upgrade melpa packages"
    This reverts commit e521d64e25d2904935112656d001c4c4e86e88aa.

---
+++ .emacs.d/elpa/find-file-in-project-5.7.7/find-file-in-project-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/harish/.emacs.d/elpa/find-file-in-project-5.7.7/find-file-in-project.el  -*- no-byte-compile: t -*-
+(define-package "find-file-in-project" "5.7.7" "Find file/directory and review Diff/Patch/Commit efficiently everywhere" '((ivy "0.10.0") (emacs "24.4")) :commit "85f0fc4ab29e4339732c19be314a4920888803b2" :keywords '("project" "convenience") :authors '(("Phil Hagelberg, Doug Alcorn, and Will Farrington")) :maintainer '("Chen Bin" . "chenbin.sh@gmail.com") :url "https://github.com/technomancy/find-file-in-project")
