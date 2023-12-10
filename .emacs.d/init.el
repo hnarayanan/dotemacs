13f6cbbf9754bdc97f785e9b0a22502c3e6b6ae8
Harish Narayanan <hnarayanan@gmail.com>
2023-12-10 18:55:02 +0000

    Clean up the init file and get the British spell-check to work

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,13 +1,13 @@
 ;; .emacs.d/init.el
 
 ;; disable loading of "default.el"
-(setq inhibit-default-init t)
+(setq-default inhibit-default-init t)
 
 ;; remove splash screen on start-up
-(setq inhibit-startup-screen t)
+(setq-default inhibit-startup-screen t)
 
 ;; hide scratch message on start-up
-(setq initial-scratch-message "")
+(setq-default initial-scratch-message "")
 
 ;; set environment variables
 (setenv "LC_ALL" "C")
@@ -18,68 +18,63 @@
 (scroll-bar-mode 0)
 
 ;; set command key to meta
-(setq mac-command-modifier 'meta)
+(setq-default mac-command-modifier 'meta)
 
 ;; copy selected text
-(setq mouse-drag-copy-region t)
-
-;; default to text-mode on start-up
-(setq initial-major-mode 'text-mode)
+(setq-default mouse-drag-copy-region t)
 
 ;; default to text-mode
-(setq default-major-mode 'text-mode)
+(setq-default initial-major-mode 'text-mode)
+(setq-default default-major-mode 'text-mode)
 
 ;; enable column number mode
-(setq column-number-mode t)
+(setq-default column-number-mode t)
 
 ;; enable visual feedback on selections
-(setq transient-mark-mode t)
+(setq-default transient-mark-mode t)
 
 ;; show the boundaries of the file
 (setq-default indicate-buffer-boundaries 'right)
 
 ;; split buffers horizontally when opening multiple files
-(setq split-width-threshold 0)
+(setq-default split-width-threshold 0)
 
 ;; don't require two spaces after full stops to define sentences
-(setq sentence-end-double-space nil)
+(setq-default sentence-end-double-space nil)
 
 ;; default to better frame titles
-(setq frame-title-format
+(setq-default frame-title-format
       (concat  "%b - emacs@" system-name))
 
 ;; show trailing spaces and empty lines
-(setq-default highlight-tabs t)
 (setq-default show-trailing-whitespace t)
 (setq-default indicate-empty-lines t)
-
-;; turn on interactive do
-(ido-mode t)
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
 
 ;; enable up- and down-casing
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 
-;; prevent extraneous tabs and use 4 spaces
+;; prevent extraneous tabs and use 2 spaces
 (setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
+(setq-default tab-width 2)
+
+;; highlight matching pairs of parentheses
+(setq-default show-paren-delay 0)
+(show-paren-mode)
 
 ;; set default indentation for different languages
 (setq c-default-style "bsd"
       c-basic-offset 2)
 (setq sgml-basic-offset 2)
 
-;; highlight matching pairs of parentheses
-(setq show-paren-delay 0)
-(show-paren-mode)
+;; turn on interactive do
+(ido-mode t)
+(setq-default ido-enable-flex-matching t)
+(setq-default ido-everywhere t)
 
-;; enable flyspell-mode
+;; enable flyspell-mode with an appropriate dictionary
 (add-hook 'text-mode-hook 'flyspell-mode)
-
-;; use British English spellings
-;; (ispell-change-dictionary "british" t)
+(setq ispell-dictionary "british")
 
 ;; setup ediff to have a neater layout
 (setq ediff-split-window-function 'split-window-horizontally)
@@ -103,6 +98,10 @@
 (use-package php-mode :ensure t)
 (use-package markdown-mode :ensure t)
 (use-package yaml-mode :ensure t)
+
+;; TODO: The interactive do (ido) used above needs to be replaced by
+;; some combination of Consult, Vertico, Embark and Marginalia,
+;; Selectrum?, Prescient/Orderless?
 
 ;; setup corfu
 (use-package corfu
