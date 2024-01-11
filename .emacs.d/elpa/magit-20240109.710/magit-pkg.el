c45f20a08873aca60fd811d5e821a094cf83d039
Harish Narayanan <hnarayanan@gmail.com>
2024-01-11 09:23:44 +0000

    Upgrade melpa packages

--- .emacs.d/elpa/magit-20240109.710/magit-pkg.el
+++ .emacs.d/elpa/magit-20240109.710/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20231204.1839" "A Git porcelain inside Emacs."
+(define-package "magit" "20240109.710" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (compat "29.1.4.4")
     (dash "20221013")
@@ -7,7 +7,7 @@
     (seq "2.24")
     (transient "20231204")
     (with-editor "20230917"))
-  :commit "51fc450b6eb440381708bdfcade351c9efc31f2d" :authors
+  :commit "c3b7fd7dc43dd87468a86aef3d59576ad79fbc16" :authors
   '(("Marius Vollmer" . "marius.vollmer@gmail.com")
     ("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
