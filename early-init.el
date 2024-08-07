91dffe7dd2b741a098efb67b60019e8d54bafa1c
Harish Narayanan <hnarayanan@gmail.com>
2024-08-07 10:56:08 +0100

    Only set the background colour when loaded in graphics mode

--- early-init.el
+++ early-init.el
@@ -4,8 +4,9 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-(setq default-frame-alist
-      ;; bg-main from the modus-vivendi-tinted theme is hard-coded below
-      (append default-frame-alist '((background-color . "#0d0e1c"))))
+(when (display-graphic-p)
+  (setq default-frame-alist
+        ;; bg-main from the modus-vivendi-tinted theme is hard-coded below
+        (append default-frame-alist '((background-color . "#0d0e1c")))))
 
 (setq-default mac-command-modifier 'meta)
