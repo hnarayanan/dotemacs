e4dd4379f25ab11eb9aee3f97e742ffaadb3facc
Harish Narayanan <hnarayanan@gmail.com>
2024-11-10 17:14:05 +0000

    Try an alternate approach to solve native compilation errors

--- early-init.el
+++ early-init.el
@@ -9,7 +9,10 @@
         ;; bg-main from the modus-vivendi-tinted theme is hard-coded below
         (append default-frame-alist '((background-color . "#0d0e1c")))))
 
-(setq comp-libgccjit-path "/opt/local/lib/gcc14/libgccjit.dylib")
-(setq native-comp-driver-options '("-L/opt/local/lib/gcc14" "-I/opt/local/include/gcc14"))
+;; (setq comp-libgccjit-path "/opt/local/lib/gcc14/libgccjit.dylib")
+;; (setq native-comp-driver-options '("-L/opt/local/lib/gcc14" "-I/opt/local/include/gcc14"))
+(setq exec-path (append '("/opt/local/bin") exec-path))
+(setenv "PATH" (concat "/opt/local/bin:" (getenv "PATH")))
+(setenv "LIBRARY_PATH" (concat "/opt/local/lib/gcc14:" (getenv "LIBRARY_PATH")))
 
 (setq-default mac-command-modifier 'meta)
