9ccaf38e862105bc15ab77d9864ca9d4add9b6dd
Harish Narayanan <hnarayanan@gmail.com>
2026-01-09 16:24:48 +0000

    Remove unnecessary :ensure t, since this is auto-ensured

--- init.el
+++ init.el
@@ -153,7 +153,6 @@
   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
 
 (use-package tree-sitter-langs
-  :ensure t
   :after tree-sitter)
 
 ;; configure a development environment for python
